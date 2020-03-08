8f1ba26237eb6fbff8d938262d8b6f02ca5ed124
d3sreveR <d3sreveR@.github.com>
2020-03-08 14:07:37 +0100

    first homework

---
+++ Homework 01/Task 02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter four numbers.");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = int.Parse(Console.ReadLine());
+            int summed = n1 + n2 + n3 + n4;
+
+            Console.WriteLine("Average:" + summed/4);
+        }
+    }
+}
---
+++ Homework 01/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input two numbers");
+            int nubmerOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine("chose what to do: + , - , * , / ");
+            string op = Console.ReadLine();
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(nubmerOne + numberTwo);
+                    break;
+
+                case "-":
+                    Console.WriteLine(nubmerOne - numberTwo);
+                    break;
+
+                case "*":
+                    Console.WriteLine(nubmerOne * numberTwo);
+                    break;
+
+                case "/":
+                    Console.WriteLine(nubmerOne / numberTwo);
+                    break;
+
+                default:
+                    Console.WriteLine("It can't be done =)");
+                    break;
+            }
+        }
+    }
+}
